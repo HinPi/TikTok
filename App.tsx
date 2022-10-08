f941b711a57cd175245fa45e85f6b9d0ac255654
HinPi <lmhiep.peel@gmail.com>
2022-10-08 17:22:13 +0700

    add splash

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { AppRouting } from './src/routing';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <AppRouting />;
 };
 
